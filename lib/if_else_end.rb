27d633745f80bdc9c0df867c5655e9aac5533eef
mirthful-generator-6192 <hortenciaecisneros@gmail.com>
2020-07-14 19:38:05 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -15,8 +15,8 @@ current_time.to_i
 # else
 #   puts "Odd!"
 # end
-current_time = Time.now
-current_time = current_time.to_i
+# current_time = Time.now
+# current_time = current_time.to_i
  
 if current_time % 2 == 0
   puts "Even!"
