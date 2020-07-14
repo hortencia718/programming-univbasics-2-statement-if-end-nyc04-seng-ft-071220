ceb78220e2d151191de80ec8afc4c60a01e35355
mirthful-generator-6192 <hortenciaecisneros@gmail.com>
2020-07-14 18:58:06 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -5,3 +5,11 @@ else
   puts "odd!"
 end
 # Write your solution here
+current_time = Time.now
+current_time =
+current_time.to_i
+ if cuurent_time % 2 == log10
+   puts "Even!"
+ else
+   puts "odd!"
+ end
